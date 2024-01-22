ce8f12c12e46bfa9fa6ce68832decae65813a01f
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-01-22 15:32:15 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "To ensure your bot runs 24/7 on Replit, you can use services like Uptime Robot to ping this URL
+    return "To ensure your bot runs 24/7 on Replit, you can use services like Uptime Robot to ping this URL"
 
 
 def run():
