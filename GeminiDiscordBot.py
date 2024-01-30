9a2af344fcde352ce0ec9247c8070ea7fd48744f
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-01-30 16:40:03 +0100

    Update GeminiDiscordBot.py

--- GeminiDiscordBot.py
+++ GeminiDiscordBot.py
@@ -245,29 +245,29 @@ async def change_settings(interaction, apply: bool, new_system_prompt: str = Sys
     with open("GeminiBotConfig.py", "r") as file:
           filedata = file.read()
 
-        # Replace the old value of the variable with the new value
-        new_content = re.sub(r'System_Prompt\s*=\s*".*?"', f'System_Prompt = "{new_system_prompt}"', filedata)
-        new_content = re.sub(r'Bot_Info\s*=\s*".*?"', f'Bot_Info = "{new_bot_info}"', filedata)
-        new_content = re.sub(r'MAX_HISTORY\s*=\s*\d+', f'MAX_HISTORY = {format(new_max_history, ".0f")}', new_content)
-        new_content = re.sub(r'Temperature_Text\s*=\s*\d+\.\d+', f'Temperature_Text = {format(new_temperature_text, ".1f")}', new_content)
-        new_content = re.sub(r'Top_P_Text\s*=\s*\d+', f'Top_P_Text = {format(new_top_p_text, ".0f")}', new_content)
-        new_content = re.sub(r'Top_K_Text\s*=\s*\d+', f'Top_K_Text = {format(new_top_k_text, ".0f")}', new_content)
-        new_content = re.sub(r'Max_Output_Tokens_Text\s*=\s*\d+', f'Max_Output_Tokens_Text = {format(new_max_output_tokens_text, ".0f")}', new_content)
-        new_content = re.sub(r'Temperature_Image\s*=\s*\d+\.\d+', f'Temperature_Image = {format(new_temperature_image, ".1f")}', new_content)
-        new_content = re.sub(r'Top_P_Image\s*=\s*\d+', f'Top_P_Image = {format(new_top_p_image, ".0f")}', new_content)
-        new_content = re.sub(r'Top_K_Image\s*=\s*\d+', f'Top_K_Image = {format(new_top_k_image, ".0f")}', new_content)
-        new_content = re.sub(r'Max_Ouptut_Tokens_Image\s*=\s*\d+', f'Max_Ouptut_Tokens_Image = {format(new_max_output_tokens_image, ".0f")}', new_content)
+          # Replace the old value of the variable with the new value
+          new_content = re.sub(r'System_Prompt\s*=\s*".*?"', f'System_Prompt = "{new_system_prompt}"', filedata)
+          new_content = re.sub(r'Bot_Info\s*=\s*".*?"', f'Bot_Info = "{new_bot_info}"', filedata)
+          new_content = re.sub(r'MAX_HISTORY\s*=\s*\d+', f'MAX_HISTORY = {format(new_max_history, ".0f")}', new_content)
+          new_content = re.sub(r'Temperature_Text\s*=\s*\d+\.\d+', f'Temperature_Text = {format(new_temperature_text, ".1f")}', new_content)
+          new_content = re.sub(r'Top_P_Text\s*=\s*\d+', f'Top_P_Text = {format(new_top_p_text, ".0f")}', new_content)
+          new_content = re.sub(r'Top_K_Text\s*=\s*\d+', f'Top_K_Text = {format(new_top_k_text, ".0f")}', new_content)
+          new_content = re.sub(r'Max_Output_Tokens_Text\s*=\s*\d+', f'Max_Output_Tokens_Text = {format(new_max_output_tokens_text, ".0f")}', new_content)
+          new_content = re.sub(r'Temperature_Image\s*=\s*\d+\.\d+', f'Temperature_Image = {format(new_temperature_image, ".1f")}', new_content)
+          new_content = re.sub(r'Top_P_Image\s*=\s*\d+', f'Top_P_Image = {format(new_top_p_image, ".0f")}', new_content)
+          new_content = re.sub(r'Top_K_Image\s*=\s*\d+', f'Top_K_Image = {format(new_top_k_image, ".0f")}', new_content)
+          new_content = re.sub(r'Max_Ouptut_Tokens_Image\s*=\s*\d+', f'Max_Ouptut_Tokens_Image = {format(new_max_output_tokens_image, ".0f")}', new_content)
 
-        # Make a PUT request to the GitHub API to update the contents of the file
-        data = {
-            "message": "Update variable value",
-            "content": base64.b64encode(new_content.encode("utf-8")).decode("utf-8"),
-            "sha": response_json["sha"]
-        }
-        headers = {
-            "Authorization": "Bearer <access-token>"
-        }
-        put_response = requests.put("https://api.github.com/repos/Nick088Official/Gemini-AI-Discord-Bot/contents/GeminiBotConfig.py", data=json.dumps(data), headers=headers)
+          # Make a PUT request to the GitHub API to update the contents of the file
+          data = {
+              "message": "Update variable value",
+              "content": base64.b64encode(new_content.encode("utf-8")).decode("utf-8"),
+              "sha": response_json["sha"]
+          }
+          headers = {
+              "Authorization": "Bearer <access-token>"
+          }
+          put_response = requests.put("https://api.github.com/repos/Nick088Official/Gemini-AI-Discord-Bot/contents/GeminiBotConfig.py", data=json.dumps(data), headers=headers)
 
         await interaction.response.send_message(str(interaction.user.name) + f" Has Changed Bots Settings! Please do /reset to instantly make the changes work.")
         print((str(interaction.user.id) + f" Has Changed Bots Settings! Please do /reset to instantly make the changes work."))
